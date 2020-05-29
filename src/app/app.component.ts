8ae90b7f73d554e29576e28f3d10d5d75669e874
codexmaker <codexmaker12@gmail.com>
2020-05-29 09:39:21 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation, OnInit} from '@angular/core';
+import { QrScannerComponent } from 'angular2-qrscanner';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-qrscanner';
+
+  @ViewChild(QrScannerComponent, { static : false }) qrScannerComponent: QrScannerComponent ;
+
+  ngOnInit() {
+        
+    }
+
+    ngAfterViewInit(): void {
+      
+      this.qrScannerComponent.getMediaDevices().then(devices => {
+        console.log(devices);
+        const videoDevices: MediaDeviceInfo[] = [];
+        for (const device of devices) {
+            if (device.kind.toString() === 'videoinput') {
+                videoDevices.push(device);
+            }
+        }
+        if (videoDevices.length > 0){
+            let choosenDev;
+            for (const dev of videoDevices){
+                if (dev.label.includes('front')){
+                    choosenDev = dev;
+                    break;
+                }
+            }
+            if (choosenDev) {
+                this.qrScannerComponent.chooseCamera.next(choosenDev);
+            } else {
+                this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+            }
+        }
+    });
+
+    this.qrScannerComponent.capturedQr.subscribe(result => {
+        console.log(result);
+    });
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgQrScannerModule } from 'angular2-qrscanner';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgQrScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
